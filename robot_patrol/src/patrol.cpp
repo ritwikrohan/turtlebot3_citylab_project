1ac39b86f2e4da6b8ce9a30bd513b753223617c8
Ritwik Rohan <ritwikrohan7@gmail.com>
2023-10-10 21:34:18 +0000

    RESUBMISSION CP5- FINAL ROBOT AND SIMULATION

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -17,7 +17,7 @@ public:
         laser_subscriber_ = this->create_subscription<sensor_msgs::msg::LaserScan>("/scan", 10, std::bind(&Patrol::laserCallback, this, std::placeholders::_1),options1);
         velocity_publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
         direction_ = 0.0;
-        timer_ = this->create_wall_timer(std::chrono::milliseconds(100),std::bind(&Patrol::controlLoop, this), timer_callback_group_);
+        timer_ = this->create_wall_timer(std::chrono::milliseconds(50),std::bind(&Patrol::controlLoop, this), timer_callback_group_);
     }
 
 private:
@@ -32,39 +32,32 @@ private:
 
     void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr laser_data) {
         std::vector<float> ranges = laser_data->ranges;
-
-        int num_ranges = ranges.size();
-        int window_size = 80;  
+        
+        int window_size = 100;  
         double safest_angle = 0.0;
-        double max_average_distance = 0.0;
+        double max_minimum_distance = -1.0;  
 
         for (int i = 180; i < 540; ++i) {
             int start_index = i - window_size / 2;
             int end_index = i + window_size / 2;
+            
+            double min_distance = std::numeric_limits<double>::max(); 
 
-            if (start_index < 0) start_index = 0;
-            if (end_index >= num_ranges) end_index = num_ranges - 1;
-
-
-            double average_distance = 0.0;
             for (int j = start_index; j <= end_index; ++j) {
                 double range = ranges[j];
-                if (!std::isinf(range)) {
-                    average_distance += range;
+                if (!std::isinf(range) && range < min_distance) {
+                    min_distance = range;
                 }
             }
-            average_distance /= (end_index - start_index + 1);
 
-
-            if (average_distance > max_average_distance) {
-                max_average_distance = average_distance;
+            if (min_distance > max_minimum_distance) {
+                max_minimum_distance = min_distance;
                 safest_angle = laser_data->angle_min + i * laser_data->angle_increment;
-                this->index = i;
             }
         }
 
         direction_ = safest_angle;
-        RCLCPP_DEBUG(this->get_logger(), "Safe set at angle and max average distance [%f, %f, %d]", direction_, max_average_distance, index);
+        RCLCPP_DEBUG(this->get_logger(), "Safe set at angle and max minimum distance [%f, %f]", direction_, max_minimum_distance);
     }
 
 
@@ -73,8 +66,8 @@ private:
 
         geometry_msgs::msg::Twist velocity_msg;
         velocity_msg.linear.x = 0.1; 
-        velocity_msg.angular.z = (direction_/1.5); // simulation (COMMENT WHEN USING ACTUAL ROBOT)
-        // velocity_msg.angular.z = (direction_/2.0); // actual robot  (UNCOMMENT WHEN USING ACTUAL ROBOT)
+        // velocity_msg.angular.z = (direction_/1.5); // simulation (COMMENT WHEN USING ACTUAL ROBOT)
+        velocity_msg.angular.z = (direction_/2.0); // actual robot  (UNCOMMENT WHEN USING ACTUAL ROBOT)
         RCLCPP_DEBUG(this->get_logger(), "safe pub = %f", direction_/2);
         velocity_publisher_->publish(velocity_msg);
     }
@@ -90,3 +83,12 @@ int main(int argc, char **argv) {
     rclcpp::shutdown();
     return 0;
 }
+
+
+
+
+
+
+
+
+
